14e94493212b6d24bc9929fdaada299568dbbe09
ironbat24 <31138073+ironbat24@users.noreply.github.com>
2021-10-20 03:45:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ client=commands.Bot(command_prefix='!',intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run('NTc0ODA4OTIzNTYzODg0NTQ1.XM-yhg.hN9r6AGAE3bo2XR42jJfbozWBq4')
+client.run('discord token')
